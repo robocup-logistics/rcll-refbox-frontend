438870d371c667f2e258f201472e8f975f0fb1b8
Ivaylo Doychev <doychev@kbsg.rwth-aachen.de>
2020-03-02 12:45:23 +0100

    components: add radio buttons and mockup to HeaderCentralInformation

--- frontend/src/components/HeaderCentralInformation.vue
+++ frontend/src/components/HeaderCentralInformation.vue
@@ -1,13 +1,29 @@
 <template>
-  <div class="col-md-2">
-    <div class="pause-play-time">
-      <div class="radio-pause-play">
-        <i class="far fa-pause-circle"></i>
-        <span>20:30</span>
+  <div class="col-md-2 border">
+    <div class="pause-play-time mt-2">
+      <div class="radio-pause-play row justify-content-center align-items-center">
+        <div class="pause-play-container mr-2">
+          <a class="btn p-0" >
+            <i class="far fa-play-circle fa-2x "></i>
+          </a>
+          <a class="btn p-0"> 
+            <i class="far fa-pause-circle fa-2x"></i>
+          </a>  
+        </div>
+        <div class="time">
+          <h3>20:30</h3>
+        </div>
       </div>
     </div>
-    <div class="phase">
 
+    <div class="phase row justify-content-center mt-1">
+      <a class="btn p-0">
+        <i class="fas fa-chevron-left fa-2x"></i>
+      </a>
+      <h3 >Production</h3>
+      <a class="btn p-0">
+        <i class="fas fa-chevron-right fa-2x"></i>
+      </a>
     </div>
   </div>
 </template>
@@ -18,6 +34,8 @@ export default {
 }
 </script>
 
-<style>
-
+<style scoped>
+h3 {
+  margin-bottom: 0px;
+}
 </style>
