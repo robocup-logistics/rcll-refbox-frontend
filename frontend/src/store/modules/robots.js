e7838d8e6c1285712dcd2ff3aacd73071570d8ca
idoychev <doychev@kbsg.rwth-aachen.de>
2020-03-04 14:11:24 +0100

    modules: add store action to fetch cyan robots

---
+++ frontend/src/store/modules/robots.js
@@ -0,0 +1,27 @@
+import { get } from '@/api/api'
+
+export default{
+  state: {
+    robotsCyan: [],
+    robotsMagenta: []
+  },
+  getters: {
+
+  },
+  mutations: {
+    setCyanRobots(state, data) {
+      state.robotsCyan = data;
+    }
+  },
+  actions: {
+    async fetchCyanRobots({commit, rootState}) {
+      try {
+        const response = await get('/robots', {team: rootState.nameTeamCyan});
+        const data = response.data;
+        commit('setCyanRobots', data);
+      } catch(error) {
+        console.log(error);
+      }
+    }
+  }
+}
